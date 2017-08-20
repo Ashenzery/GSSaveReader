ea9b1f25cb0fc73185c94439885ec2e224112635
Ashenzery <daniil.stepanenko.96@bk.ru>
2017-08-20 15:45:39 +0300

    added some img

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from cx_Freeze import setup, Executable
+﻿from cx_Freeze import setup, Executable
 
 setup(
     name = 'GSSaveReader',   #Name of exe file
-    version = "0.0.0.3",
+    version = "0.0.0.4",
     description = u'Удобный просмотр ваших сохранений',
     executables = [Executable('main.py')]
 )
