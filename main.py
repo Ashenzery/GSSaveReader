3f962aa1f6b7a5bd4dd7509ac9d11c9dd28723ae
Daniil <kareek.exe@gmail.com>
2017-08-21 23:48:59 +0300

    add some labels

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from Tkinter import *
 from PIL import Image, ImageTk
 
 
+class MyLabel(Label):
+    def __init__(self, place, text, x = 0, y = 0, color = 'white', font = 'comic-sans 10'):
+        Label.__init__(self, place)
+        widget = Label(place, bg = 'black', fg = color, font = font, text = text)
+        widget.place(x = x, y = y)
+
+
 class GoblinButton:
     def __init__(self, name, y_pos, x_pos, root):
         self.frame = Frame(root, bd = 0, bg = "black")
@@ -43,16 +50,6 @@ class GoblinWindow:
         #self.win.overrideredirect( True )
         self.win.title( name )
         self.win.tkraise( )
-        self.labels = {
-            'lvl'   : Label(self.win),
-            'gold'  : Label(self.win),
-            'item1' : Label(self.win),
-            'item2' : Label(self.win),
-            'item3' : Label(self.win),
-            'item4' : Label(self.win),
-            'item5' : Label(self.win),
-            'item6' : Label(self.win)
-        }
         self.buttons = {
             're'    :  Button(
                                 self.win,
@@ -63,13 +60,13 @@ class GoblinWindow:
             'up'    :  Button(
                                 self.win,
                                 image = images['up'],
-                                #command = ,
+                                command = self.up_action,
                                 bd = 0,
                                 bg = 'black'  ),
             'down'  : Button(
                                 self.win,
                                 image = images['down'],
-                                #command = ,
+                                command = self.down_action,
                                 bd = 0,
                                 bg = 'black'  ),
             'copy' : Button(
@@ -83,10 +80,38 @@ class GoblinWindow:
         self.buttons['up'].place(x=575, y=65)
         self.buttons['down'].place(x=575, y=165)
         self.buttons['copy'].place(x=575, y=115)
+        self.saves = os.listdir("../../{0}".format(name))
+        self.saves = [ x for x in self.saves if ( x[:5] == '-load' and x[-4:] == '.txt' ) ]
+        self.i = 0
+        self.saves_len = len(self.saves)
+        self.lvl = '9'
+        self.gold = '3000'
+        self.items = [{'name' : 'Item' + str(i+1), 'color' : 'white'} for i in range(6) ]#TODO
+        self.labels = {
+            'lvl'   : MyLabel(self.win, 'Уровень: {0}'.format(self.lvl), y = 84, x = 250, font = 'comic-sans 7' ),
+            'gold'  : MyLabel(self.win, 'Золото: {0}'.format(self.gold), y = 84, x = 350, font = 'comic-sans 7' ),
+            'item1' : MyLabel(self.win, self.items[0]['name'], y = 101, x= 250, color = self.items[0]['color'] ),
+            'item2' : MyLabel(self.win, self.items[1]['name'], y = 117, x= 250, color = self.items[1]['color'] ),
+            'item3' : MyLabel(self.win, self.items[2]['name'], y = 133, x= 250, color = self.items[2]['color'] ),
+            'item4' : MyLabel(self.win, self.items[3]['name'], y = 150, x= 250, color = self.items[3]['color'] ),
+            'item5' : MyLabel(self.win, self.items[4]['name'], y = 167, x= 250, color = self.items[4]['color'] ),
+            'item6' : MyLabel(self.win, self.items[5]['name'], y = 184, x= 250, color = self.items[5]['color'] )
+        }
+
+    def up_action(self):
+            self.i = (self.i + 1) %  self.saves_len
+
+    def down_action(self):
+            if self.i == 0:
+                self.i = self.saves_len
+            else:
+                self.i -= 1
+
 
     def hide_all(self):
         for goblin in goblin_arr:
             goblin.window.win.withdraw( )
+
     def write(self):
         root.clipboard_clear()
         root.clipboard_append('txt to write')
@@ -106,7 +131,7 @@ def get_right_down(scr, app_width=330, app_height=210):
         app_width,
         app_height,
         int( width - app_width),
-        int( height - app_height) - 65
+        int( height - app_height)
         #int( ( width / 2.0 ) - ( app_width / 2.0 ) ),
         #int( ( height / 2.0 ) - ( app_height / 2.0 ) ),
         )
@@ -133,8 +158,9 @@ def get_left_of(scr, app_width=733 , app_height=210 ):
     )
 
 work_dir = module_path( )
-
+print
 print work_dir
+print len( work_dir )
 
 root = Tk( )
 root.resizable(False, False)
@@ -161,7 +187,6 @@ bg2 = ImageTk.PhotoImage( bg2_img )
 stop_btm_frame = Frame( root, bd=0, bg="black" )
 stop_btm_frame.place( x=238, y=153 )
 
-#TODO: criate copy.png
 images = { name : ImageTk.PhotoImage(
                 Image.open( "./temp/{0}.png".format(name) )
                 ) for name in ('up', 'down', 'copy', 're', 'stop') }
@@ -208,10 +233,11 @@ gobins = [
 
 goblin_arr = []
 for goblin in gobins:
-    goblin_arr.append(
-        GoblinButton(goblin['name'], goblin['y'], goblin['x'], root)
-    )
+    if os.path.isdir('../../{0}'.format(goblin['name'])):
+        goblin_arr.append(
+            GoblinButton(goblin['name'], goblin['y'], goblin['x'], root)
+                        )
 
 
-
+#print goblin_arr[0].window.saves
 root.mainloop( )
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name = 'GSSaveReader',   #Name of exe file
-    version = "0.0.0.7",
+    version = "1.0.0.0",
     description = u'Удобный просмотр ваших сохранений',
     executables = [Executable('main.py')]
 )
